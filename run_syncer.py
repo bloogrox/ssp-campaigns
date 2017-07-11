3466f0d5f98615e9ee80b188f9b17de20a8bef2f
aslan <bloogrox@gmail.com>
2017-07-11 17:43:09 +0300

    take correct env var

--- run_syncer.py
+++ run_syncer.py
@@ -1,8 +1,10 @@
 from nameko.cli.shell import make_nameko_helper
 from nameko.constants import AMQP_URI_CONFIG_KEY
-import os
 
-config = {AMQP_URI_CONFIG_KEY: os.environ["RABBITMQ_BIGWIG_URL"]}
+import settings
+
+
+config = {AMQP_URI_CONFIG_KEY: settings.AMQP_URI}
 
 n = make_nameko_helper(config)
 
