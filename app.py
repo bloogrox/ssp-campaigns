bcc3ba8094d3fa42c5d33333813e5f3975f4aaa4
aslan <bloogrox@gmail.com>
2017-07-15 16:45:21 +0300

    set timer interval 20 sec

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ class Timer:
     campaigns_runner_service = RpcProxy("campaigns_runner_service")
     syncer_service = RpcProxy("syncer_service")
 
-    @timer(interval=1)
+    @timer(interval=20)
     def run_campaigns(self):
         print("tick")
         self.campaigns_runner_service.run.call_async()
