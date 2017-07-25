3b8544991082683cd01b26f66a55d5093961f292
aslan <bloogrox@gmail.com>
2017-07-25 20:30:29 +0300

    add sample local settings file

---
+++ settings/local.sample.py
@@ -0,0 +1,6 @@
+REDIS_URI = "redis://127.0.0.1:6379/0"
+MONGO_URI = "mongodb://127.0.0.1:27017/db"
+AMQP_URI = "amqp://guest:guest@127.0.0.1:5672"
+EXT_AMQP_URI = "amqp://guest:guest@127.0.0.1:5673"
+PUSHW_AUTH_TOKEN = ""
+PUSHW_AUTH_KEY = ""
