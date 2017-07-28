6f773278c836f309acf06c6571bcf0e0b74ab934
aslan <bloogrox@gmail.com>
2017-07-29 02:44:50 +0300

    fix es query

--- services/subscriber.py
+++ services/subscriber.py
@@ -12,48 +12,29 @@ class SubscriberService:
     def get_subscribers(self, countries, hours_whitelist, limit):
         print("SubscriberService.get_subscribers: getting subscribers")
         try:
-            country_filter = []
-            for country in countries:
-                country_filter.append({
-                    "match": {
-                        "country": country
-                    }
-                })
-
-            timezone_filter = []
-            timezones = [tz for tz in pytz.all_timezones if datetime.now(
-                pytz.timezone(tz)).hour in hours_whitelist]
-            for timezone in timezones:
-                timezone_filter.append({
-                    "match_phrase": {
-                        "timezone": timezone
-                    }
-                })
-
+            timezones = [tz for tz in pytz.all_timezones
+                         if (datetime
+                             .now(pytz.timezone(tz)).hour
+                             in hours_whitelist)]
             res = es.msearch(body=[
-                {"index": "subscribers"},
-                {"size": limit,
-                 "query": {
-                     "function_score": {
-                         "query": {
-                             "bool": {
-                                 "should": country_filter,
-                                 "filter": {
-                                     "bool": {
-                                         "should": timezone_filter
-                                     }
-                                 }
-                             }
-                         },
-                         "functions": [
-                             {
-                                 "random_score": {}
-                             }
-                         ]
-
-                     }
-                 }
-                 }
+                {"index": "users"},
+                {
+                    "size": limit,
+                    "query": {
+                        "function_score": {
+                            "query": {
+                                "bool": {
+                                    "must": [
+                                        {"terms": {"country": countries}},
+                                        {"terms": {"timezone": timezones}}
+                                    ]
+                                }
+                            },
+                            "random_score": {},
+                            "boost_mode": "replace"
+                        }
+                    }
+                }
             ])
             subscribers = []
             for row in res['responses'][0]['hits']['hits']:
@@ -62,7 +43,7 @@ class SubscriberService:
                 subscribers.append(subscriber)
             return subscribers
         except Exception as e:
-            print("SubscriberService.get_subscribers: Exception " + str(e))
+            print(f"SubscriberService.get_subscribers: Exception {e}")
 
     @rpc
     def update_subscriber(self, document):
