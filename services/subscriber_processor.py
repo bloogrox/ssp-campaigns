b4c9d096d12c69415e677a37ea57ce70ea152542
aslan <bloogrox@gmail.com>
2017-08-17 22:31:04 +0300

    use CachedCabinet

--- services/subscriber_processor.py
+++ services/subscriber_processor.py
@@ -1,5 +1,7 @@
+import redis
 from nameko.rpc import rpc, RpcProxy
-from cabinet import Cabinet
+from cabinet import Cabinet, CachedCabinet, RedisEngine
+from app import REDIS_POOL
 
 
 class SubscriberProcessorService:
@@ -12,8 +14,12 @@ class SubscriberProcessorService:
     def process_subscriber(self, payload):
         print("SubscriberProcessorService.process_subscriber: "
               f"processing subscriber: {payload['subscriber']}")
+        redis_client = redis.Redis(connection_pool=REDIS_POOL)
         cab = Cabinet("https://ssp-cabinet.herokuapp.com")
-        general_settings = cab.general()
+        cached_cabinet = CachedCabinet(
+            cab,
+            RedisEngine(redis_client, prefix="CABINET_CACHE", ttl=5))
+        general_settings = cached_cabinet.general()
         limit = general_settings["push_limit_per_token"]
         subscriber_pushes = (self.counter_service
                              .get_pushes_count(payload["subscriber"]["_id"]))
