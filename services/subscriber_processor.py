b6873e92d34abbcc39211552ef6965c532ad513c
aslan <bloogrox@gmail.com>
2017-08-25 20:01:10 +0300

    add execution time prints

--- services/subscriber_processor.py
+++ services/subscriber_processor.py
@@ -28,10 +28,17 @@ class SubscriberProcessorService:
         print("SubscriberProcessorService.process_subscriber: "
               f"received settings in {int(total_time)}ms")
         limit = general_settings["push_limit_per_token"]
+        start_time2 = time.time()
         subscriber_pushes = (self.counter_service
                              .get_pushes_count(payload["subscriber"]["_id"]))
+        end_time2 = time.time()
+        print("SubscriberProcessorService.process_subscriber: "
+              f"get_pushes_count in {(end_time2 - start_time2) * 1000}ms")
         if subscriber_pushes < limit:
             self.queue.publish.call_async(payload)
         else:
             print("SubscriberProcessorService.process_subscriber: "
                   f"limit for subscriber: {payload} exceeded")
+        finish_time = time.time()
+        print("SubscriberProcessorService.process_subscriber: "
+              f"total execution time {(finish_time - start_time) * 1000}ms")
