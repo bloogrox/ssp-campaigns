a7c080f5c5031d4ccf0b444a6b92bd9b35190863
aslan <bloogrox@gmail.com>
2017-09-13 02:15:59 +0300

    set last bid time in redis

--- services/subscriber_processor.py
+++ services/subscriber_processor.py
@@ -40,18 +40,18 @@ class SubscriberProcessorService:
         print("SubscriberProcessorService.process_subscriber: "
               f"get_pushes_count in {(end_time2 - start_time2) * 1000}ms")
         has_quota = subscriber_pushes < limit
-        last_push_key = f"subscriber:{token}:last-push-at"
-        last_push_time = redis_client.get(last_push_key)
-        if last_push_time:
-            time_passed = time.time() - last_push_time
+        last_bid_key = f"subscriber:{token}:last-bid-at"
+        last_bid_time = redis_client.get(last_bid_key)
+        if last_bid_time:
+            time_passed = time.time() - last_bid_time
             time_passed_enough = time_passed > (bid_interval * 60)
             print("SubscriberProcessorService.process_subscriber: "
-                  f"passed time since last push {time_passed}")
+                  f"passed time since last bid {time_passed}")
         else:
             time_passed_enough = True
         if has_quota and time_passed_enough:
             self.queue.publish.call_async(payload)
-            redis_client.set(last_push_key, int(time.time()), ex=DAY_SECONDS)
+            redis_client.set(last_bid_key, int(time.time()), ex=DAY_SECONDS)
         else:
             print("SubscriberProcessorService.process_subscriber: "
                   f"for subscriber: {payload} "
