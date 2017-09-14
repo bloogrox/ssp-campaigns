20c0b44a282b63481fd5f21ddec727ff7312aefd
aslan <bloogrox@gmail.com>
2017-09-14 19:26:32 +0300

    remove syncer code

--- bin/start_syncer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-nameko run --config nameko.configs/syncer.yml \
-    services.syncer:SyncerService \
-    services.syncer_subscriber_augmentor:SyncerSubscriberAugmentorService \
-    services.syncer_page_processor:SyncerPageProcessorService \
-    services.subscriber_remote_storage:SubscriberRemoteStorageService
