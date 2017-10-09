131d23d8cd84147d1e67e8354d640fb6d871d911
aslan <bloogrox@gmail.com>
2017-10-09 20:18:15 +0300

    use logger

--- services/campaigns_runner.py
+++ services/campaigns_runner.py
@@ -2,7 +2,7 @@ import redis
 from nameko.rpc import rpc, RpcProxy
 import settings
 from cabinet import Cabinet, CachedCabinet, RedisEngine
-from app import REDIS_POOL
+from app import REDIS_POOL, logger
 
 
 class CampaignsRunnerService:
@@ -18,8 +18,8 @@ class CampaignsRunnerService:
             cab,
             RedisEngine(redis_client, prefix="CABINET_CACHE", ttl=5))
         campaigns = cached_cabinet.campaigns()
+        logger.debug("CampaignsRunnerService.run: "
+                     f"received active campaigns {campaigns}")
         for campaign in campaigns:
-            print(f"CampaignsRunnerService.run: sending campaign: {campaign}"
-                  " for processing.")
             (self.campaign_processor_service.process_campaign
              .call_async(campaign))
