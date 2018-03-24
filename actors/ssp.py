7f873e231b607d94894cdd5a1516180930fe779d
aslan <bloogrox@gmail.com>
2018-03-24 22:24:26 +0300

    add try-except

--- actors/ssp.py
+++ actors/ssp.py
@@ -6,15 +6,18 @@ from app import logger
 
 class SSP(pykka.ThreadingActor):
     def on_receive(self, payload):
-        payload["subscriber"]["token"] = payload["subscriber"]["_id"]
-        data = {
-            "dsp": payload["campaign"]["dsp"],
-            "subscriber": payload["subscriber"]
-        }
-        resp = requests.post(settings.SSP_URL + "/sell/", json=data)
+        try:
+            payload["subscriber"]["token"] = payload["subscriber"]["_id"]
+            data = {
+                "dsp": payload["campaign"]["dsp"],
+                "subscriber": payload["subscriber"]
+            }
+            resp = requests.post(settings.SSP_URL + "/sell/", json=data)
 
-        if resp.status_code != 202:
-            raise Exception("SSP response: "
-                            f"{resp.status_code} {resp.content}")
+            if resp.status_code != 202:
+                raise Exception("SSP response: "
+                                f"{resp.status_code} {resp.content}")
 
-        logger.debug(f"SSP: ssp response {resp.content}")
+            logger.debug(f"SSP: ssp response {resp.content}")
+        except Exception as e:
+            logger.error(f"SSP exception: {e}")
